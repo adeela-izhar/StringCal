a35e73529241c1d508719d1a5483344763a7f05e
Adeela Izhar <adeela.izhar@comviva.com>
2022-05-16 00:43:14 +0530

    Added my project

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static  void main(String... arg){
+        Scanner s = new Scanner(System.in);
+        String inputText = s.next();
+        System.out.println(Add(inputText));
+    }
+
+    public static int Add(String input) throws NullPointerException {
+        try {
+
+
+        } catch (NullPointerException ne) {
+            return 0;
+        } catch (Exception e) {
+
+        }
+        return 0;
+    }
+}
